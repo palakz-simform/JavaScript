3e33542ab453bf41f179a1d809d8f738ca0b7c9b
Palak Zalavadia <palak.z@simformsolutions.com>
2023-02-03 09:15:01 +0530

    Updated log and ln function

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,13 +1,13 @@
 const actions = document.querySelector(".actions");
 const ans = document.getElementById("input");
-ans.value=0
+ans.value = 0;
 let expression = "";
 memory = 0;
 actions.addEventListener("click", (e) => {
   const value = e.target.value || e.target.parentElement.value;
 
   if (value !== undefined) {
-    ans.value="";
+    ans.value = "";
     switch (value) {
       case "DEG":
       case "RAD":
@@ -16,7 +16,7 @@ actions.addEventListener("click", (e) => {
 
       case "fe":
         expression = parseFloat(expression).toExponential();
-        forNaN(expression)
+        forNaN(expression);
         break;
 
       case "mc":
@@ -37,9 +37,9 @@ actions.addEventListener("click", (e) => {
         break;
 
       case "m+":
-          memory = memory + expression;
-          expression = 0;
-          break;
+        memory = memory + expression;
+        expression = 0;
+        break;
 
       case "m-":
         memory = memory - expression;
@@ -84,15 +84,17 @@ actions.addEventListener("click", (e) => {
         expression = Math.PI * expression;
         forNaN(expression);
         break;
- 
+
       case "ce":
         expression = "";
         ans.value = 0;
-       
+
         break;
 
-      case "clear":        
-        expression = expression.toString().substring(0,expression.toString().length-1)        
+      case "clear":
+        expression = expression
+          .toString()
+          .substring(0, expression.toString().length - 1);
         break;
 
       case "square":
@@ -101,77 +103,86 @@ actions.addEventListener("click", (e) => {
         break;
 
       case "1/x":
-        expression = "1/";        
+        expression = "1/";
         break;
 
       case "|x|":
         expression = Math.abs(expression);
-        forNaN(expression)
+        forNaN(expression);
         break;
 
       case "exp":
         expression = Math.exp(expression);
-        forNaN(expression)
+        forNaN(expression);
         break;
 
       case "sqrt":
-        expression="√"          
+        expression = "√";
         break;
 
       case "*(":
         let expp = expression.toString().slice(-1);
-        if(expp==='+'||expp==='*'||expp==='-'||expp==='/'||expp===''){
-          expression+="("
+        if (
+          expp === "+" ||
+          expp === "*" ||
+          expp === "-" ||
+          expp === "/" ||
+          expp === ""
+        ) {
+          expression += "(";
+        } else {
+          expression += "*(";
         }
-        else{
-          expression+="*("
-        }       
         break;
 
       case "factorial":
-        let fact = 1
-        if(isNaN(expression)){
-           expression="Error"
-        }           
-        else{
+        let fact = 1;
+        if (isNaN(expression)) {
+          expression = "Error";
+        } else {
           for (i = 1; i <= expression; i++) {
-             fact *= i;
+            fact *= i;
           }
-           expression = fact;
-          }          
-          break;
+          expression = fact;
+        }
+        break;
 
       case "=":
         removezero();
-        try{
-         if (expression.includes("√")){
-          squareroot();
-         }
-          const answer = eval(expression);
+        try {
+          if (expression.includes("√")) {
+            squareroot();
+          }
+          else if(expression.includes("log")){
+            logten();
+          }
+          else if(expression.includes("ln")){
+            ln();
+          }
+         else{
+            const answer = eval(expression);
           expression = answer;
-        }
-        catch{
-          expression="Syntax Error"
+          }
+          
+        } catch {
+          expression = "Syntax Error";
         }
         forNaN(expression);
         break;
 
       case "10power":
-        expression = 10 ** expression;
-        forNaN(expression);
+        expression="10**"
         break;
 
       case "ln":
-        expression = Math.log(expression);
-        forNaN(expression);
+        expression = "ln"
+       
         break;
 
       case "log10":
-        expression = Math.log10(expression);
-        forNaN(expression);
-
+        expression = "log";
         break;
-        
+   
       case "sin":
         expression = Math.sin(expression);
         forNaN(expression);
@@ -196,31 +207,40 @@ actions.addEventListener("click", (e) => {
         break;
 
       case "0":
-               
-        if(expression==="0"){
-          expression=value
-        }
-        else{
-          expression+=0
+        if (expression === "0") {
+          expression = value;
+        } else {
+          expression += 0;
         }
         break;
 
       default:
         expression += value;
     }
-   
-    function removezero(){
-      if(expression.charAt(0)==='0'){
-        expression = expression.toString().substring(1,expression.toString().length) 
+
+    function removezero() {
+      if (expression.charAt(0) === "0") {
+        expression = expression
+          .toString()
+          .substring(1, expression.toString().length);
       }
-      
     }
 
-    function squareroot(){
-      let e =expression.substring(1, expression.length);
-        expression = Math.sqrt(e);
+    function logten(){
+      let e = expression.substring(3, expression.length);
+      expression = Math.log10(e);
     }
-    
+
+    function ln(){
+      let e = expression.substring(2, expression.length);
+      expression = Math.log(e);
+    }
+
+    function squareroot() {
+      let e = expression.substring(1, expression.length);
+      expression = Math.sqrt(e);
+    }
+
     function degrad() {
       var t = document.getElementById("rd");
 
@@ -231,15 +251,14 @@ actions.addEventListener("click", (e) => {
         expression = expression * (Math.PI / 180);
         t.value = "DEG";
       }
-      forNaN(expression)
+      forNaN(expression);
     }
 
-    function forNaN(exp){
-      if(isNaN(exp)){
-        expression="Error"
-      }
-      else{
-        expression = exp
+    function forNaN(exp) {
+      if (isNaN(exp)) {
+        expression = "Error";
+      } else {
+        expression = exp;
       }
     }
 
