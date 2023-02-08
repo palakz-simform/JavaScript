5de9ed60629e8b1c01b4f304019b8d94456232ab
Palak Zalavadia <palak.z@simformsolutions.com>
2023-02-08 09:48:42 +0530

    Minor changes in UI

--- SinglePageApplication/script.js
+++ SinglePageApplication/script.js
@@ -1,11 +1,11 @@
 let flag = 0;
-
+slideshow(flag);
 function controller(x) {
   flag = flag + x;
   slideshow(flag);
 }
 
-slideshow(flag);
+
 function slideshow(num) {
   let slides = document.getElementsByClassName("slide");
   if (num == slides.length) {
